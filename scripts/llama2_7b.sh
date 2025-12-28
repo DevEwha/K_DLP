e1f3c309b9c76e5a250c404393657f3db993a16d
rinarina0429 <forrina0429@gmail.com>
2025-12-28 11:18:40 +0900

    Chore: llama2 7b 프루닝 실험용 실행 스크립트 정리

--- scripts/llama2_7b.sh
+++ scripts/llama2_7b.sh
@@ -7,6 +7,11 @@ ALPHA=0.15
 PRUNE_METHOD="wanda_dlp"
 SPARSITY_RATIO=0.7
 SPARSITY_TYPE="unstructured"
+SAVE_MODEL="/acpl-ssd32/k_models/llama2_7b_${PRUNE_METHOD}_${SPARSITY_RATIO}_${SPARSITY_TYPE}_alpha${ALPHA}"
+
+# sparsity 0.7 unstructured with alpha 0.15
+# sparsity 0.6 unstructured with alpha 0.1
+# sparsity 0.5 unstructured with alpha 0.04
 
 python   run.py \
     --model $MODEL \
@@ -14,30 +19,4 @@ python   run.py \
     --prune_method $PRUNE_METHOD \
     --sparsity_ratio $SPARSITY_RATIO \
     --sparsity_type $SPARSITY_TYPE \
-
-# sparsity 0.7 unstructured with alpha 0.15
-python   run.py \
-    --model "meta-llama/Llama-2-7b-hf" \
-    --alpha 0.15 \
-    --prune_method "wanda_dlp" \
-    --sparsity_ratio 0.7 \
-    --sparsity_type "unstructured" \
-    --save_model "outputs/llama2_7b_wanda_dlp_0.7_unstructured_alpha0.15"
-
-# sparsity 0.6 unstructured with alpha 0.1
-python run.py \
-    --model "meta-llama/Llama-2-7b-hf" \
-    --alpha 0.1 \
-    --prune_method "wanda_dlp" \
-    --sparsity_ratio 0.6 \
-    --sparsity_type "unstructured" \
-    --save_model "outputs/llama2_7b_wanda_dlp_0.6_unstructured_alpha0.1"
-
-# sparsity 0.5 unstructured with alpha 0.04
-python run.py \
-    --model "meta-llama/Llama-2-7b-hf" \
-    --alpha 0.04 \
-    --prune_method "wanda_dlp" \
-    --sparsity_ratio 0.5 \
-    --sparsity_type "unstructured" \
-    --save_model "outputs/llama2_7b_wanda_dlp_0.5_unstructured_alpha0.04"
+    --save_model $SAVE_MODEL \
